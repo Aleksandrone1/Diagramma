4aea12476aa818f4833d4ce1f8ea36d3358c74b8
Aleksandrone1 <alpeeffalex@yandex.ru>
2023-04-02 10:38:40 +0300

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+module Diagramma {
+    requires java.desktop;
+    requires java.sql;
+    requires javafx.fxml;
+    requires  javafx.graphics;
+    requires javafx.controls;
+    requires javafx.swing;
+    requires mssql.jdbc;
+    requires java.mail;
+    opens sample;
+}
